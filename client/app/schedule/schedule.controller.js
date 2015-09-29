26d920089dbd0cefab4e08d988de6e63fe772392
liebeskind <daniel.liebeskind@gmail.com>
2015-09-29 18:09:54 -0400

    Calendar now makes any classes in the past unavailable.  Also adds trainer name in

--- client/app/schedule/schedule.controller.js
+++ client/app/schedule/schedule.controller.js
@@ -3,17 +3,32 @@
 angular.module('bodyAppApp')
   .controller('ScheduleCtrl', function ($scope, $http, socket, $firebaseArray) {
     var todayDate = new Date()
-    console.log(todayDate)
     var todayDayOfWeek = todayDate.getDay()
     
     var dd = todayDate.getDate()
     var mm = todayDate.getMonth()
     var yy = todayDate.getFullYear()
 
+    var sunDayWeek = 0
+    var monDayWeek = 1
+    var tuesDayWeek = 2
+    var wedDayWeek = 3
+    var thursDayWeek = 4
+    var friDayWeek = 5
+    var satDayWeek = 6
+
+    var sunDate = todayDate.getDate() + sunDayWeek - todayDayOfWeek
+    var monDate = todayDate.getDate() + monDayWeek - todayDayOfWeek
+    var tuesDate = todayDate.getDate() + tuesDayWeek - todayDayOfWeek
+    var wedDate = todayDate.getDate() + wedDayWeek - todayDayOfWeek
+    var thursDate = todayDate.getDate() + thursDayWeek - todayDayOfWeek
+    var friDate = todayDate.getDate() + friDayWeek - todayDayOfWeek
+    var satDate = todayDate.getDate() + satDayWeek - todayDayOfWeek
+
     function getDayFormatter(day) {
-        var sunDate = new Date()
-        sunDate.setDate(todayDate.getDate() + day - todayDayOfWeek)
-        return sunDate.getMonth() + 1 + "/" + sunDate.getDate()
+        var newDate = new Date()
+        newDate.setDate(todayDate.getDate() + day - todayDayOfWeek)
+        return newDate.getMonth() + 1 + "/" + newDate.getDate()
     }
 
     // connect to firebase 
@@ -22,224 +37,318 @@ angular.module('bodyAppApp')
 
     // three way data binding
     // syncObject.$bindTo($scope, 'days');
-      
+
     $scope.days = {
         sunday: {
             name: 'Sun',
-            date: getDayFormatter(0),
+            dayOfWeek: sunDayWeek,
+            isToday: todayDayOfWeek == sunDayWeek,
+            date: sunDate,
+            formattedDate: getDayFormatter(sunDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
         },
 
         monday: {
             name: 'Mon',
-            date: getDayFormatter(1),
+            dayOfWeek: monDayWeek,
+            isToday: todayDayOfWeek == monDayWeek,
+            date: monDate,
+            formattedDate: getDayFormatter(monDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: true,
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
 
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
         },
           
         tuesday: {
             name: 'Tues',
-            date:  getDayFormatter(2),
+            dayOfWeek: tuesDayWeek,
+            isToday: todayDayOfWeek == tuesDayWeek,
+            date: tuesDate,
+            formattedDate:  getDayFormatter(tuesDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
         },
           
         wednesday: {
             name: 'Wed',
-            date: getDayFormatter(3),
+            dayOfWeek: wedDayWeek,
+            isToday: todayDayOfWeek == wedDayWeek,
+            date: wedDate,
+            formattedDate: getDayFormatter(wedDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
         },
 
         thursday: {
             name: 'Thurs',
-            date: getDayFormatter(4),
+            dayOfWeek: thursDayWeek,
+            isToday: todayDayOfWeek == thursDayWeek,
+            date: thursDate,
+            formattedDate: getDayFormatter(thursDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
         },
 
         friday: {
             name: 'Fri',
-            date: getDayFormatter(5),
+            dayOfWeek: friDayWeek,
+            isToday: todayDayOfWeek == friDayWeek,
+            date: friDate,
+            formattedDate: getDayFormatter(friDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
         },
         saturday: {
             name: 'Sat',
-            date: getDayFormatter(6),
+            dayOfWeek: satDayWeek,
+            isToday: todayDayOfWeek == satDayWeek,
+            date: satDate,
+            formattedDate: getDayFormatter(satDayWeek),
             slots: {
               0800: {
                 time: '8:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1100: {
                 time: '11:00am',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
               1600: {
                 time: '4:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               },
-                2000: {
+              2000: {
                 time: '8:00pm',
                 booked: false,
                 trainer: "J. Mendelson",
-                unavailable: false
+                unavailable: false,
+                spots: 8,
+                spotsTaken: 0,
               }
             }
+          }
+    }
+
+    //Makes slots in the past unavailable
+    for (var day in $scope.days) {
+      if ($scope.days[day].dayOfWeek < todayDayOfWeek) {
+        for (var slot in $scope.days[day].slots) {
+          $scope.days[day].slots[slot].unavailable = true;
         }
+      }
+      //Makes slots earlier today unavailable
+      if ($scope.days[day].dayOfWeek == todayDayOfWeek) {
+        for (var slot in $scope.days[day].slots) {
+          if (slot < todayDate.getHours()*100) {
+            $scope.days[day].slots[slot].unavailable = true;
+          }
+        } 
+      }
     }
 
     // function to set the default data
