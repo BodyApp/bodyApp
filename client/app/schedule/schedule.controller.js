807607c5fe14bbf641da09c6df25fdbb17e45fb4
liebeskind <daniel.liebeskind@gmail.com>
2015-09-30 15:27:45 -0400

    Can now filter out unavailable classes

--- client/app/schedule/schedule.controller.js
+++ client/app/schedule/schedule.controller.js
@@ -1,14 +1,30 @@
 'use strict';
 
 angular.module('bodyAppApp')
-  .controller('ConsumerScheduleCtrl', function ($scope, $http, socket, $location, $firebaseObject, Auth, Schedule) {
-    
+    .controller('ConsumerScheduleCtrl', function ($scope, $http, socket, $location, $firebaseObject, Auth, Schedule) {
+        var currentUser = Auth.getCurrentUser();
+        var currentUserEmail = currentUser.email
+        console.log(currentUser)
+        $scope.currentUser = currentUser;
+        
+        Schedule("weekof9272015").$bindTo($scope, 'days')
 
-    var currentUser = Auth.getCurrentUser();
-    var currentUserEmail = currentUser.email
-    console.log(currentUser)
-    $scope.currentUser = currentUser;
+        $scope.availableClasses = true
 
-    Schedule("weekof9272015").$bindTo($scope, 'days')
-});
+    })
+
+    .filter('showAvailable', function() {
+      return function(input, showAvailable) {
+        console.log(input)
+        console.log(showAvailable)
+        if (showAvailable) {
+            for (var slot in input) {
+                if (input[slot].past) {
+                    input[slot].hidden = true;
+                }
+            }
+        }
+        return input
+      };
+    })
 
