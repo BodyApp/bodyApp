0e9759098767a815bd2021fb7dbbc5bba9e88a9f
liebeskind <daniel.liebeskind@gmail.com>
2015-10-02 13:58:36 -0400

    Changes main redirect to dashboard.  If not authenticated, will direct to login page.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -4,7 +4,7 @@ angular.module('bodyAppApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('main', {
-        url: '/',
+        url: '/main',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
--- client/app/schedule/schedule.js
+++ client/app/schedule/schedule.js
@@ -4,7 +4,7 @@ angular.module('bodyAppApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('schedule', {
-        url: '/schedule',
+        url: '/',
         templateUrl: 'app/schedule/consumerSchedule.html',
         controller: 'ConsumerScheduleCtrl',
         authenticate: true
