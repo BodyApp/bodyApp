4346057c912cbe649045aba1272fe6ee289fc164
liebeskind <daniel.liebeskind@gmail.com>
2015-12-07 14:53:01 -0500

    Fixes cronjob, which wasn't yet using the full date format

--- server/app.js
+++ server/app.js
@@ -51,7 +51,10 @@ new CronJob('29 * * * * *', function() {
 	var todayDate = new Date();
   var sunDate = new Date();
   sunDate.setDate(todayDate.getDate() - todayDate.getDay());
-  var weekOf = "weekof"+(sunDate.getMonth()+1)+sunDate.getDate()+sunDate.getFullYear()
+  var sunGetDate = sunDate.getDate();
+  var sunGetMonth = sunDate.getMonth()+1;
+  var sunGetYear = sunDate.getFullYear();
+  var weekOf = "weekof"+ (sunGetMonth<10?"0"+sunGetMonth:sunGetMonth) + (sunGetDate<10?"0"+sunGetDate:sunGetDate) + sunGetYear;
 	var weeklyFirebaseRef = new Firebase("https://bodyapp.firebaseio.com/"+(weekOf));
 
 	weeklyFirebaseRef.once("value", function(currentWeek) {
