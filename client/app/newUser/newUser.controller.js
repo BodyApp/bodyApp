ef87edffe4d77e45b92a3f465256896ee45bd0bd
liebeskind <daniel.liebeskind@gmail.com>
2015-12-11 18:36:53 -0500

    Forces user to enter something into 'injuries' before they can progress.

--- client/app/newUser/newUser.controller.js
+++ client/app/newUser/newUser.controller.js
@@ -3,6 +3,7 @@
 angular.module('bodyAppApp')
   .controller('NewUserCtrl', function ($scope, $http, $state, User, Auth) {
     $scope.newUserStep = 1;
+    $scope.errorDiv = false
 
     $scope.incrementStep = function() {
     	$scope.newUserStep++;
@@ -14,9 +15,15 @@ angular.module('bodyAppApp')
     }
 
     $scope.saveInjuries = function(injuryString) {
-    	User.saveInjuries({id: Auth.getCurrentUser()}, {injuryString: injuryString}).$promise.then(function(confirmation) {
-			  console.log("Successfully saved injury info.");
-		  })
-		  $scope.newUserStep++;
+    	injuryString = injuryString || ""
+    	if (injuryString.length < 2) {
+    		$scope.errorDiv = true
+    		console.log("Didn't enter any information!")
+    	} else {
+    		User.saveInjuries({id: Auth.getCurrentUser()}, {injuryString: injuryString}).$promise.then(function(confirmation) {
+				  console.log("Successfully saved injury info.");
+			  })
+			  $scope.newUserStep++;	
+    	}
     }
   });
