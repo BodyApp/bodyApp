f8680bc1b61a4bb03256b515921b5af48bc969f0
liebeskind <daniel.liebeskind@gmail.com>
2016-01-08 15:58:26 -0500

    Saves results to public list if elect to publish publicly

--- client/app/results/classFeedback.controller.js
+++ client/app/results/classFeedback.controller.js
@@ -1,9 +1,25 @@
 'use strict';
 
 angular.module('bodyAppApp')
-  .controller('ClassFeedbackCtrl', function ($scope, Schedule, Auth, User) {
+  .controller('ClassFeedbackCtrl', function ($scope, $location, Schedule, Auth, User) {
   	$scope.classCompleted = Schedule.classUserJustJoined;
   	console.log($scope.classCompleted)
+
+  	if (!$scope.classCompleted) {
+  		$location.path('/')
+  	}
+
+  	var classDate = new Date($scope.classCompleted.date);        
+    var sunDate = new Date();
+    sunDate.setDate(classDate.getDate() - classDate.getDay());
+    var sunGetDate = sunDate.getDate();
+    var sunGetMonth = sunDate.getMonth()+1;
+    var sunGetYear = sunDate.getFullYear();
+    var weekOf = "weekof"+ (sunGetMonth<10?"0"+sunGetMonth:sunGetMonth) + (sunGetDate<10?"0"+sunGetDate:sunGetDate) + sunGetYear;
+    var weekOfRef = new Firebase("https://bodyapp.firebaseio.com/" + weekOf)
+    var dayRef = weekOfRef.child(classDate.getDay())
+
+    var currentUser = Auth.getCurrentUser()
 
   	$scope.submitRatingAndResults = function(rating, score, comment, postToPublic) {
   		
@@ -18,7 +34,7 @@ angular.module('bodyAppApp')
 			// 	console.log("rating saved.  New trainer rating: " + data.rating + " on " + data.numRatings + " ratings.")
 			// })
 
-			User.addRating({ id: Auth.getCurrentUser()._id }, {
+			User.addRating({ id: currentUser._id }, {
         trainer: $scope.classCompleted.trainer._id, 
         rating: rating
       }, function(data) {
@@ -28,7 +44,27 @@ angular.module('bodyAppApp')
       }).$promise;
 
   		if (postToPublic) {
-
+  			dayRef.child("resultList").push({
+  				score: score,
+  				comment: comment,
+  				userId: currentUser._id,
+  				userFirstName: currentUser.firstName,
+  				userLastName: currentUser.lastName,
+  				userPicture: currentUser.picture,
+  				timePosted: (new Date()).getTime()
+  			})
   		}
+
+  	// 	User.saveResult({ id: Auth.getCurrentUser()._id }, {
+   //      score: score, 
+   //      comment: comment,
+   //      wod: "WodData",
+   //      date: classDate.getTime(),
+   //      weekOf: weekOf
+   //    }, function(data) {
+   //      console.log("Result saved: " + data)
+   //    }, function(err) {
+   //        console.log(err)
+   //    }).$promise;
   	}
   });
