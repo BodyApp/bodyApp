4e64b407223475585cd746dcaf30b4f2dd758d1e
liebeskind <daniel.liebeskind@gmail.com>
2016-02-06 19:37:58 -0500

    Changes results classFeedback structure

--- client/app/results/classFeedbackModal.controller.js
+++ client/app/results/classFeedbackModal.controller.js
@@ -12,16 +12,16 @@ angular.module('bodyAppApp')
 
   	var classDate = new Date($scope.classCompleted.date);        
   	var classKey = ""+classDate.getFullYear()+""+((classDate.getMonth()+1 < 10)?"0"+(classDate.getMonth()+1):classDate.getMonth()+1)+""+((classDate.getDate() < 10)?"0"+classDate.getDate():classDate.getDate())
-    var sunDate = new Date(classDate.getFullYear(), classDate.getMonth(), classDate.getDate() - classDate.getDay(), 11, 0, 0);
+    // var sunDate = new Date(classDate.getFullYear(), classDate.getMonth(), classDate.getDate() - classDate.getDay(), 11, 0, 0);
     // var sunDate = new Date();
     // sunDate.setDate(classDate.getDate() - classDate.getDay());
-    var sunGetDate = sunDate.getDate();
-    var sunGetMonth = sunDate.getMonth()+1;
-    var sunGetYear = sunDate.getFullYear();
-    var weekOf = "weekof"+ sunGetYear + (sunGetMonth<10?"0"+sunGetMonth:sunGetMonth) + (sunGetDate<10?"0"+sunGetDate:sunGetDate);
+    // var sunGetDate = sunDate.getDate();
+    // var sunGetMonth = sunDate.getMonth()+1;
+    // var sunGetYear = sunDate.getFullYear();
+    // var weekOf = "weekof"+ sunGetYear + (sunGetMonth<10?"0"+sunGetMonth:sunGetMonth) + (sunGetDate<10?"0"+sunGetDate:sunGetDate);
     var ref = new Firebase("https://bodyapp.firebaseio.com/")
-    var weekOfRef = ref.child("classes").child(weekOf)
-    var dayRef = weekOfRef.child(DayOfWeekSetter.setDay(classDate.getDay()))
+    // var weekOfRef = ref.child("classes").child(weekOf)
+    // var dayRef = weekOfRef.child(DayOfWeekSetter.setDay(classDate.getDay()))
 
     var currentUser = Auth.getCurrentUser()
 
@@ -33,103 +33,105 @@ angular.module('bodyAppApp')
     })
 
   	$scope.submitRatingAndResults = function(rating, score, comment, feedback, postToPublic, minutes, seconds) {
-  		console.log(score);
-  		console.log(minutes);
-	  	if (!rating) rating = 5.0
-  		if (!score) score = ""
-  		if (!comment) comment = ""
-  		if (!feedback) feedback = ""
+      if (!rating) rating = 5.0
+      if (!score) score = ""
+      if (!comment) comment = ""
+      if (!feedback) feedback = ""
 
-  		var priority;
-  		if ($scope.classWod.scoreType.id === 0) {
-  			score = seconds + minutes*60
-  			score = score * 1
-  			priority = score
-  		} else {
-  			score = score * 1
-  			priority = -score
-  		}
+      var priority;
+      if ($scope.classWod.scoreType.id === 0) {
+        score = seconds + minutes*60
+        score = score * 1
+        priority = score
+      } else {
+        score = score * 1
+        priority = -score
+      }
 
-  		//Add result to user object
-  		User.saveResult({ id: Auth.getCurrentUser()._id }, {
+      //Add result to user object
+      User.saveResult({ id: Auth.getCurrentUser()._id }, {
         score: score,
         typeOfScore: $scope.classWod.classType, 
         comment: comment,
         feedback: feedback,
         wod: "WodData", //This needs to be changed once the day's wod data has been implemented
         dateTime: classDate.getTime(),
-        weekOf: weekOf,
         date: classKey
       }, function(data) {
-        console.log(data.user);
         Auth.updateUser(data.user);
+        ref.child("resultsByUser").child(currentUser._id).child(classKey).update({
+          score: score*1,
+          comment: comment,
+          classDateTime: $scope.classCompleted.date,
+          classDayKey: classKey,
+          timePosted: (new Date()).getTime()        
+        }, function(err) {if (!err) console.log("results saved to user's profile")})
       }, function(err) {
-          console.log(err)
+        console.log(err)
       }).$promise.then(function() {
-      	//Add rating to trainer object
-      	User.addRating({ id: currentUser._id }, {
-	        trainer: $scope.classCompleted.trainer._id, 
-	        rating: rating
-	      }, function(data) {
-	        console.log("rating saved.  New trainer rating: " + data.trainerRating + " on " + data.trainerNumRatings + " ratings.")
-	        $uibModalInstance.close()
-	      }, function(err) {
-	          console.log(err)
-	      }).$promise;	
+        //Add rating to trainer object
+        User.addRating({ id: currentUser._id }, {
+          trainer: $scope.classCompleted.trainer._id, 
+          rating: rating
+        }, function(data) {
+          console.log("rating saved.  New trainer rating: " + data.trainerRating + " on " + data.trainerNumRatings + " ratings.")
+          $uibModalInstance.close()
+        }, function(err) {
+            console.log(err)
+        }).$promise;  
       });
 
       //Post result to public list
-  		if (postToPublic) {
-  			var dayList;
-  			var classList;
-  			dayList = dayRef.child("resultList").push({
-  				score: score*1,
-  				comment: comment,
-  				userId: currentUser._id,
-  				userFirstName: currentUser.firstName,
-  				userLastName: currentUser.lastName,
-  				userPicture: currentUser.picture,
-  				classDateTime: $scope.classCompleted.date,
-  				timePosted: (new Date()).getTime()				
-  			}, function(error) {
-  				if (error) return console.log(error);
-  				console.log("Result successfully published to public list.")
+      if (postToPublic) {
+        var classList;
+        var dayList = ref.child("resultsByDay").child(classKey).push({
+          score: score*1,
+          comment: comment,
+          userId: currentUser._id,
+          userFirstName: currentUser.firstName,
+          userLastName: currentUser.lastName,
+          userPicture: currentUser.picture,
+          classDateTime: $scope.classCompleted.date,
+          timePosted: (new Date()).getTime()        
+        }, function(error) {
+          if (error) return console.log(error);
+          console.log("Result successfully published to public list.")
 
-  				// if ($scope.classWod.scoreType.id === 0) {
-  				// 	dayList.setPriority(score);
-  				// } else {
-  				// 	dayList.setPriority(-score);
-  				// }
+          // if ($scope.classWod.scoreType.id === 0) {
+          //  dayList.setPriority(score);
+          // } else {
+          //  dayList.setPriority(-score);
+          // }
 
-	  			classList = dayRef.child('slots').child($scope.classCompleted.date).child("classResultsList").push({
-	  				score: score,
-	  				comment: comment,
-	  				userId: currentUser._id,
-	  				userFirstName: currentUser.firstName,
-	  				userLastName: currentUser.lastName,
-	  				userPicture: currentUser.picture,
-	  				timePosted: (new Date()).getTime()				
-	  			}, function(error) {
-	  				if (error) return console.log(error);
-	  				if (feedback.length > 2) {
-	  					ref.child('feedback').push({
-	  						userId: currentUser._id,
-	  						userFirstName: currentUser.firstName,
-	  						timePosted: (new Date()).getTime(),
-	  						classTaken: classDate.getTime(),
-	  						instructor: $scope.classCompleted.trainer._id,
-	  						rating: rating,
-	  						feedback: feedback
-	  					})
-	  				}
-	  				console.log("Result successfully published to class list.")
-	   			// 	if ($scope.classWod.scoreType.id === 0) {
-	  				// 	classList.setPriority(score);
-	  				// } else {
-	  				// 	classList.setPriority(-score);
-	  				// }
-	  			}).setPriority(priority)
-  			}).setPriority(priority)
-  		}
-  	}
+          classList = ref.child("resultsByClass").child($scope.classCompleted.date).push({
+            score: score,
+            comment: comment,
+            userId: currentUser._id,
+            userFirstName: currentUser.firstName,
+            userLastName: currentUser.lastName,
+            userPicture: currentUser.picture,
+            timePosted: (new Date()).getTime()        
+          }, function(error) {
+            if (error) return console.log(error);
+            if (feedback.length > 2) {
+              ref.child('feedback').push({
+                userId: currentUser._id,
+                userFirstName: currentUser.firstName,
+                timePosted: (new Date()).getTime(),
+                classTaken: classDate.getTime(),
+                instructor: $scope.classCompleted.trainer._id,
+                rating: rating,
+                feedback: feedback
+              })
+            }
+            console.log("Result successfully published to class list.")
+          //  if ($scope.classWod.scoreType.id === 0) {
+            //  classList.setPriority(score);
+            // } else {
+            //  classList.setPriority(-score);
+            // }
+          }).setPriority(priority)
+        }).setPriority(priority)
+      }
+    }
   });
