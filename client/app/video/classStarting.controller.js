0639cf034af14b633ce469cd33084f5698cbbc27
liebeskind <daniel.liebeskind@gmail.com>
2016-02-25 15:59:12 -0500

    Fixes bug where every user was being transformed into an admin

--- client/app/video/classStarting.controller.js
+++ client/app/video/classStarting.controller.js
@@ -118,14 +118,14 @@ angular.module('bodyAppApp')
       // $scope.instructorPicUrl = $scope.instructor.picture
 
       //Network Test
-      if (currentUser._id != classToJoin.trainer._id && currentUser.role != 'admin') {
+      if (currentUser._id != classToJoin.trainer._id) {
         $scope.networkTestCountdown = timeoutMs + 6000
         $scope.testingNetwork = true;
         $timeout(function() {
           console.log("Prevented standstill at loading screen.")
           $scope.testingNetwork = false;
         }, $scope.networkTestCountdown)
-        
+
         conductInternetTest(classToJoin.sessionId);
       }
 
--- server/config/seed.js
+++ server/config/seed.js
@@ -48,7 +48,7 @@ User.find({}).remove(function() {
             "max": 17
         }
     },
-    role: "admin",
+    role: "user",
     __v: 51,
     // injuries: "Nope",
     bookedIntroClass: true,
