a90eeffbe694cdbcac652081a4f059655d7a9481
liebeskind <daniel.liebeskind@gmail.com>
2016-03-07 12:52:57 -0500

    Adds product hunt landing page.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,4 +8,13 @@ angular.module('bodyAppApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
-  });
+  })
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('producthunt', {
+        url: '/producthunt',
+        templateUrl: 'app/main/producthunt.html',
+        controller: 'MainCtrl'
+      });
+  })
+  ;
