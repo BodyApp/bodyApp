92af6d71231b02a00fe720efa3a7c5a37b4aa801
liebeskind <daniel.liebeskind@gmail.com>
2016-05-07 16:00:59 -0400

    Added delete subscription plan endpoint

--- server/api/studio/studio.controller.js
+++ server/api/studio/studio.controller.js
@@ -11,39 +11,69 @@ var mailgun = new Mailgun({apiKey: api_key, domain: domain});
 var Firebase = require('firebase');
 var ref = new Firebase("https://bodyapp.firebaseio.com/");
 
-exports.createSubsciptionPlan = function(req, res, next){
-  var studioId = req.body.studioId
-  var amount = req.body.amount
-  var name = req.body.name
-  var currency = req.body.currency
-  var interval = req.body.interval
-  var statement_descriptor = req.body.statement_descriptor
-  var metadata = req.body.metadata
-  var userThatCreatedPlan = req.body.userThatCreatedPlan
-  
-  stripe.plans.create({
-    id: studioId + amount/100 + "v" + new Date().getTime(),
-    amount: amount,
-    interval: interval,
-    name: name,
-    currency: currency,
-    statement_descriptor: statement_descriptor,
-    metadata: {
-      "studioId": studioId,
-      "userThatCreatedPlan": userThatCreatedPlan,
+exports.createSubscriptionPlan = function(req, res, next){
+  var studioId = req.body.studioId;
+  var amount = req.body.amount;
+  var name = req.body.name;
+  var currency = req.body.currency;
+  var interval = req.body.interval;
+  var statement_descriptor = req.body.statement_descriptor;
+  var metadata = req.body.metadata;
+  var userThatCreatedPlan = req.body.userThatCreatedPlan;
+
+  ref.child('studios').child(studioId).child("stripeConnected").child('stripe_user_id').once('value', function(snapshot) {
+    if (snapshot.exists()) {
+      // stripe = require("stripe")(snapshot.val());
+
+      stripe.plans.create({
+        id: studioId + amount/100 + "v" + new Date().getTime(),
+        amount: amount,
+        interval: interval,
+        name: name,
+        currency: currency,
+        statement_descriptor: statement_descriptor,
+        metadata: {
+          "studioId": studioId,
+          "userThatCreatedPlan": userThatCreatedPlan,
+        }
+      }, {
+        stripe_account: snapshot.val()
+      }, function(err, plan) {
+        if (err) {console.log(err); return res.status(400).send(err);}
+        ref.child('studios').child(studioId).child('subscriptionPlans').child(plan.id).update(plan, function(err) {
+          if (err) return res.status(400).send(err);
+          res.status(200).json(plan);  
+        })
+      });
+    } else {
+      return res.status(400).send("Your studio doesn't have a valid account ID associated with it.");
     }
-  }, function(err, plan) {
-    if (err) {console.log(err); return res.status(400).send(err);}
-    ref.child('studios').child(studioId).child('subscriptionPlans').child(plan.id).update(plan, function(err) {
-      if (err) return res.status(400).send(err)
-      res.status(200).json(plan);  
-    })
-  });
+  })
 };
 
+exports.deleteSubscriptionPlan = function(req, res, next){
+  var studioId = req.body.studioId;
+  var planId = req.body.planId;
 
+  ref.child('studios').child(studioId).child("stripeConnected").child('stripe_user_id').once('value', function(snapshot) {
+    if (snapshot.exists()) {
 
-router.put('/:id/deleteSubscriptionPlan', auth.isAuthenticated(), controller.deleteSubscriptionPlan);
+      stripe.plans.del(planId,
+      {
+        stripe_account: snapshot.val()
+      }, function(err, plan) {
+        if (err) {console.log(err); return res.status(400).send(err);}
+        ref.child('studios').child(studioId).child('subscriptionPlans').child(plan.id).remove(function(err) {
+          if (err) return res.status(400).send(err);
+          res.status(200).send("Plan " + planId + " successfully deleted");  
+        })
+      });
+    } else {
+      return res.status(400).send("Your studio doesn't have a valid account ID associated with it.");
+    }
+  })
+};
+
 router.put('/:id/listSubscriptionPlans', auth.isAuthenticated(), controller.listSubscriptionPlans);
 router.put('/:id/listActiveSubscriptions', auth.isAuthenticated(), controller.listActiveSubscriptions);
 router.put('/:id/listCoupons', auth.isAuthenticated(), controller.listCoupons);
