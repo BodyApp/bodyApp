a72dc2772716362f4169961b8c5050bb3175852b
liebeskind <daniel.liebeskind@gmail.com>
2016-05-09 21:53:09 -0400

    Can now add class types.  Also makes sure there is a name and description and that description isn't too long.

--- client/app/studios/classes.controller.js
+++ client/app/studios/classes.controller.js
@@ -18,20 +18,37 @@ angular.module('bodyAppApp')
     //   { name: "Spain", flag: "Spain.png" },
     //   { name: "Germany", flag: "Germany.png" },
     // ];
+
+    $scope.saveClassType = function(classToSave) {
+      console.log(classToSave)
+      if (!classToSave.className) return $scope.missingName = true;
+      // if (!classToSave.equipment) return $scope.missingEquipment = true
+      if (!classToSave.classDescription) return $scope.missingDescription = true;
+      if (classToSave.classDescription.length > 200) return $scope.descriptionTooLong = classToSave.classDescription.length;
+      ref.child("classTypes").push(classToSave, function(err) {
+        if (err) return console.log(err);
+        console.log("Class successfully saved")
+        $scope.classToCreate = {};
+        $scope.missingName = false;
+        $scope.missingDescription = false;
+        $scope.descriptionTooLong = false;
+        if(!$scope.$$phase) $scope.$apply();
+      })
+    }
     
     $scope.loadEquipment = function($query) {
       return [
-        { "name": "Dumbbells" },
-        { "name": "Mat" },
-        { "name": "Punching Bag" },
-        { "name": "TRX" },
-        { "name": "Chin-Up Bar" },
-        { "name": "Jump Rope" },
-        { "name": "Exercise Bands" },
-        { "name": "Core Wheel" },
-        { "name": "Foam Roller" },
-        { "name": "Exercise Ball" },
-        { "name": "Thera-Band" }
+        { "name": "Dumbbells", 'id': "sdfjweoiWE342"},
+        { "name": "Mat", 'id': "fwej22bboi8" },
+        { "name": "Punching Bag", "id": "23sdfnn98beu" },
+        { "name": "TRX", "id": "nj230abspoj"},
+        { "name": "Chin-Up Bar", "id": "b239ancBIUH30" },
+        { "name": "Jump Rope", "id": "29BAu87B2j73" },
+        { "name": "Exercise Bands", "id": "3gs1b0E8wb7" },
+        { "name": "Core Wheel", "id": "h2bf03nBaP" },
+        { "name": "Foam Roller", "id": "23bf2u3ehdi" },
+        { "name": "Exercise Ball", "id": "293hfnQ8WDA" },
+        { "name": "Thera-Band", "id": "8nf0wejde82" }
 
       ]
       return $http.get('countries.json', { cache: true}).then(function(response) {
