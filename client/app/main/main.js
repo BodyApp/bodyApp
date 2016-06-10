5979d6fa91b14628a68235598fc567c9e8233169
just1jp <jmendelson22@gmail.com>
2016-06-10 14:29:04 -0400

    finished up the sign up, sign in, and new user flow screens. These are in a special DIV to make them look correct on my localhost. However you can rip out the divs inside the MODAL CONTAINER and they should be fine within the modal you create.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -52,6 +52,14 @@ angular.module('bodyAppApp')
   })
   .config(function ($stateProvider) {
     $stateProvider
+      .state('newSignUp', {
+        url: '/newSignUp',
+        templateUrl: 'app/main/newSignUp.html',
+        controller: 'MainCtrl'
+      });
+  })
+  .config(function ($stateProvider) {
+    $stateProvider
       .state('invite-a-friend', {
         url: '/invite-a-friend',
         templateUrl: 'app/main/invite-a-friend.html',
