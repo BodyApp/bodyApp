746e53dbc3c0827f330f64c227d913cc76d4e97d
liebeskind <daniel.liebeskind@gmail.com>
2016-07-06 13:54:10 -0400

    Begins adding new discover functionality

--- client/app/main/discover.controller.js
+++ client/app/main/discover.controller.js
@@ -7,15 +7,27 @@ angular.module('bodyAppApp')
     $scope.studioPictures = {};
 
     getStudios();
+    getDates();
 
     $scope.categoryFilter = $state.params.tag;
     Intercom('trackEvent', 'navigatedToDiscover');
+
+    $scope.classesVsStudios = "classes";
+
+    function getDates() {
+      var today = new Date();
+      $scope.today = moment(today).format('MMM Do')
+
+      var sevenDaysFromNow = new Date(today.getTime() + (7*24*60*60*1000));
+      $scope.sevenDaysFromNow = moment(sevenDaysFromNow).format('MMM Do');
+    }
 
     function getStudios() {
 	    ref.child('studioIds').orderByValue().on('value', function(snapshot) {
 	    	$scope.studios = [];
 	    	snapshot.forEach(function(studioId) {
-	    		getPicture(studioId.key)
+	    		getPicture(studioId.key);
+          getUpcomingClasses(studioId.key);
 	    		ref.child('studios').child(studioId.key).child('storefrontInfo').once('value', function(snapshot) {
 	    			$scope.studios.push(snapshot.val())
 				    if(!$scope.$$phase) $scope.$apply();
@@ -32,6 +44,28 @@ angular.module('bodyAppApp')
         console.log(error)
       });
 	  }
+  
+    function getUpcomingClasses(studioId) {
+      var rightNow = new Date().getTime().toString();
+      var sevenDaysFromNow = new Date(new Date().getTime() + (7*24*60*60*1000)).toString();
+
+      console.log(rightNow)
+
+      ref.child('studios').child(studioId).child('classes').startAt(rightNow).orderByKey().on('value', function(snapshot) {
+        snapshot.forEach(function(classToAdd) {
+          console.log(classToAdd.val())
+          var dayOfClass = new Date(classToAdd.val().dateTime).getDay();
+          var timeOfClass = classToAdd.val().dateTime
+          $scope.upcomingClasses = $scope.upcomingClasses || {};
+          $scope.upcomingClasses[dayOfClass] = $scope.upcomingClasses[dayOfClass] || {};
+          $scope.upcomingClasses[dayOfClass][timeOfClass] = $scope.upcomingClasses[dayOfClass][timeOfClass] || [];
+          $scope.upcomingClasses[dayOfClass][timeOfClass].push(classToAdd.val());
+          if(!$scope.$$phase) $scope.$apply();
+        })
+        
+      })
+    }
+
   })
 
 	.filter('filterByCategory', function() {
