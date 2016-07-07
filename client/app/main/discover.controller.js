82923c03d268c6f837b31624bf1e63b26c675ebf
liebeskind <daniel.liebeskind@gmail.com>
2016-07-06 23:45:33 -0400

    Fixed discover ordering issues

--- client/app/main/discover.controller.js
+++ client/app/main/discover.controller.js
@@ -16,11 +16,17 @@ angular.module('bodyAppApp')
 
     $scope.classesVsStudios = "classes";
 
+    $scope.days = [];
+    for (var i = 0; i < 8; i++) {
+      var classDate = new Date(new Date().getTime() + (i*24*60*60*1000))
+      $scope.days.push(new Date(classDate.getFullYear(), classDate.getMonth(), classDate.getDate(), 12, 0, 0, 0).getTime())
+    }
+
     function getDates() {
       var today = new Date();
       $scope.today = moment(today).format('MMM Do')
 
-      var sevenDaysFromNow = new Date(today.getTime() + (8*24*60*60*1000));
+      var sevenDaysFromNow = new Date(today.getTime() + (7*24*60*60*1000));
       $scope.sevenDaysFromNow = moment(sevenDaysFromNow).format('MMM Do');
     }
 
@@ -53,20 +59,20 @@ angular.module('bodyAppApp')
 
       ref.child('studios').child(studioId).child('classes').startAt(rightNow).endAt(sevenDaysFromNow).orderByKey().on('value', function(snapshot) {
         snapshot.forEach(function(classToAdd) {
-          getClassTypes(studioId, classToAdd.val().classType)
-          getInstructors(studioId, classToAdd.val().instructor)
-          var dateToUse = new Date(classToAdd.val().dateTime)
-          var dateOfClass = new Date(dateToUse.getFullYear(), dateToUse.getMonth(), dateToUse.getDate(), 12, 0, 0, 0);
-          var timeOfClass = classToAdd.val().dateTime
+          getClassTypes(studioId, classToAdd.val().classType);
+          getInstructors(studioId, classToAdd.val().instructor);
+          var dateToUse = new Date(classToAdd.val().dateTime);
+          var dateOfClass = new Date(dateToUse.getFullYear(), dateToUse.getMonth(), dateToUse.getDate(), 12, 0, 0, 0).getTime();
+          var timeOfClass = classToAdd.val().dateTime;
           $scope.upcomingClasses = $scope.upcomingClasses || {};
-          $scope.upcomingClasses[dateOfClass] = $scope.upcomingClasses[dateOfClass] || {};
-          $scope.upcomingClasses[dateOfClass][timeOfClass] = $scope.upcomingClasses[dateOfClass][timeOfClass] || [];
-          var toPush = classToAdd.val()
+          $scope.upcomingClasses[dateOfClass] = $scope.upcomingClasses[dateOfClass] || [];
+          // $scope.upcomingClasses[dateOfClass][timeOfClass] = $scope.upcomingClasses[dateOfClass][timeOfClass] || [];
+          var toPush = classToAdd.val();
           toPush.studioId = studioId;
-          $scope.upcomingClasses[dateOfClass][timeOfClass].push(toPush);
+          $scope.upcomingClasses[dateOfClass].push(toPush)
+          // $scope.upcomingClasses[dateOfClass][timeOfClass].push(toPush);
           if(!$scope.$$phase) $scope.$apply();
-        })
-        
+        })  
       })
     }
 
@@ -89,17 +95,17 @@ angular.module('bodyAppApp')
     }
 
     $scope.getDayOfWeek = function(day) {
-      var dayToFormat = new Date(day)
+      var dayToFormat = new Date(day*1)
       return moment(dayToFormat).format('ddd')
     }
 
     $scope.getDate = function(day) {
-      var dayToFormat = new Date(day)
+      var dayToFormat = new Date(day*1)
       return moment(dayToFormat).format("MMM Do")
     }
 
     $scope.getFullDate = function(day) {
-      var dayToFormat = new Date(day)
+      var dayToFormat = new Date(day*1)
       return moment(dayToFormat).format('ddd MMM Do')
     }
 
@@ -115,12 +121,25 @@ angular.module('bodyAppApp')
   })
 
 	.filter('filterByCategory', function() {
-  return function(items, field) {
-    if (field.length < 1) return items
-    var result = {};
-    angular.forEach(items, function(value, key) {
-      if (value.categories[field]) result[key] = value;
-    });
-    return result;
-  };
-});
+    return function(items, field) {
+      if (field.length < 1) return items
+      var result = {};
+      angular.forEach(items, function(value, key) {
+        if (value.categories[field]) result[key] = value;
+      });
+      return result;
+    };
+  })
+  .filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+      var filtered = [];
+      angular.forEach(items, function(item) {
+        filtered.push(item);
+      });
+      filtered.sort(function (a, b) {
+        return (a[field] > b[field] ? 1 : -1);
+      });
+      if(reverse) filtered.reverse();
+      return filtered;
+    };
+  });
