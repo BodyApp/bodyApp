5bc301e28eb678a06d1b2a819b42d76d09d90c85
liebeskind <daniel.liebeskind@gmail.com>
2016-08-06 16:59:46 -0500

    Upgrades to reservations

--- client/app/studios/classInfo.controller.js
+++ client/app/studios/classInfo.controller.js
@@ -197,8 +197,8 @@ angular.module('bodyAppApp')
 
         return $mdDialog
         .show( confirm ).then(function() {
-          ref.child("bookings").child(classId).child($scope.currentUser._id).remove()
-          firebase.database().ref().child('userBookings').child($scope.currentUser._id).child(classId).remove(function(err) {
+          ref.child("bookings").child(classId).child($scope.currentUser._id.toString()).remove()
+          firebase.database().ref().child('userBookings').child($scope.currentUser._id.toString()).child(classId).remove(function(err) {
             if (err) return console.log(err)
             Intercom('trackEvent', 'cancelledClass', {
               studioId: studioId,
@@ -215,9 +215,9 @@ angular.module('bodyAppApp')
               $location.path('/studios/' + studioId)
             });
           })
-          firebase.database().ref().child('bookingCancellations').child($scope.currentUser._id).child(classId).update({classId: classId, studioId: studioId})
-          ref.child("userBookings").child($scope.currentUser._id).child(classId).remove()
-          ref.child("cancellations").child(classId).child($scope.currentUser._id).update({firstName: $scope.currentUser.firstName, lastName: $scope.currentUser.lastName.charAt(0), timeBooked: new Date().getTime(), picture: $scope.currentUser.picture ? $scope.currentUser.picture : "", facebookId: $scope.currentUser.facebookId ? $scope.currentUser.facebookId : ""})
+          firebase.database().ref().child('bookingCancellations').child($scope.currentUser._id.toString()).child(classId).update({classId: classId, studioId: studioId})
+          ref.child("userBookings").child($scope.currentUser._id.toString()).child(classId).remove()
+          ref.child("cancellations").child(classId).child($scope.currentUser._id.toString()).update({firstName: $scope.currentUser.firstName, lastName: $scope.currentUser.lastName.charAt(0), timeBooked: new Date().getTime(), picture: $scope.currentUser.picture ? $scope.currentUser.picture : "", facebookId: $scope.currentUser.facebookId ? $scope.currentUser.facebookId : ""})
         });
         // if (confirm("Are you sure you want to cancel class?")) {
           
@@ -254,7 +254,7 @@ angular.module('bodyAppApp')
 
           analytics.track('addToWaitlist', {studioId: studioId, classId: classId});
             
-          ref.child("waitlist").child(classId).child($scope.currentUser._id).update({firstName: $scope.currentUser.firstName, lastName: $scope.currentUser.lastName.charAt(0), timeBooked: new Date().getTime(), picture: $scope.currentUser.picture ? $scope.currentUser.picture : "", facebookId: $scope.currentUser.facebookId ? $scope.currentUser.facebookId : ""}, function(err) {
+          ref.child("waitlist").child(classId).child($scope.currentUser._id.toString()).update({firstName: $scope.currentUser.firstName, lastName: $scope.currentUser.lastName.charAt(0), timeBooked: new Date().getTime(), picture: $scope.currentUser.picture ? $scope.currentUser.picture : "", facebookId: $scope.currentUser.facebookId ? $scope.currentUser.facebookId : ""}, function(err) {
             if (err) return err;
             return $mdDialog.show( alert )
           })
@@ -338,7 +338,7 @@ angular.module('bodyAppApp')
           studioId: studioId,
           classType: $scope.classDetails.classType
         });
-        firebase.database().ref().child('taughtClass').child($scope.currentUser._id).child(classId).update({
+        firebase.database().ref().child('taughtClass').child($scope.currentUser._id.toString()).child(classId).update({
           classId: classId,
           studioId: studioId,
           classType: $scope.classDetails.classType
@@ -364,7 +364,7 @@ angular.module('bodyAppApp')
           instructorFirstName: $scope.instructorDetails.firstName
         });
         $location.path('/uservideo')  
-        firebase.database().ref().child('tookClass').child($scope.currentUser._id).child(classId).update({
+        firebase.database().ref().child('tookClass').child($scope.currentUser._id.toString()).child(classId).update({
           classId: classId,
           studioId: studioId,
           classType: $scope.classDetails.classType,
@@ -544,7 +544,7 @@ angular.module('bodyAppApp')
           facebookId: $scope.currentUser.facebookId ? $scope.currentUser.facebookId : "", 
         }
         if ($rootScope.trialPeriodTime) bookingObj.freeBooking = $rootScope.trialPeriodTime;
-        ref.child("bookings").child(slot.dateTime).child($scope.currentUser._id).update(bookingObj, function(err) {
+        ref.child("bookings").child(slot.dateTime).child($scope.currentUser._id.toString()).update(bookingObj, function(err) {
           if (err) return console.log(err)
           console.log("Added booking")
         });
@@ -552,7 +552,7 @@ angular.module('bodyAppApp')
           dateTime: slot.dateTime, instructor: slot.instructor, classType: slot.classType
         }
         if ($rootScope.trialPeriodTime) userBookingObj.freeBooking = $rootScope.trialPeriodTime;
-        ref.child("userBookings").child($scope.currentUser._id).child(slot.dateTime).update(userBookingObj, function(err) {
+        ref.child("userBookings").child($scope.currentUser._id.toString()).child(slot.dateTime).update(userBookingObj, function(err) {
           if (err) return console.log(err)
           console.log("Added user booking to studio")
         });
@@ -570,7 +570,7 @@ angular.module('bodyAppApp')
 
         if ($rootScope.trialPeriodTime) userBookingsObj.freeBooking = $rootScope.trialPeriodTime;
 
-        firebase.database().ref().child('userBookings').child($scope.currentUser._id).child(slot.dateTime).update(userBookingsObj, function(err) {
+        firebase.database().ref().child('userBookings').child($scope.currentUser._id.toString()).child(slot.dateTime).update(userBookingsObj, function(err) {
           if (err) return console.log(err)
           console.log("Added user bookings to userBookings")
         })
