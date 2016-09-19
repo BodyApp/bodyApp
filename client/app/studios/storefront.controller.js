553c59b349b23779e831ded87b8a884aeeb40308
liebeskind <daniel.liebeskind@gmail.com>
2016-09-18 18:12:24 -0700

    Can now see more and see less of video library

--- client/app/studios/storefront.controller.js
+++ client/app/studios/storefront.controller.js
@@ -1009,12 +1009,12 @@ angular.module('bodyAppApp')
       var sec_num = parseInt(this, 10); // don't forget the second param
       var hours   = 0;
       // var hours   = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+      var minutes = Math.floor((sec_num - (hours * 3600)) / 60)*1;
       var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
       // if (hours   < 10) {hours   = "0"+hours;}
       // if (minutes < 10) {minutes = "0"+minutes;}
-      if (seconds < 10) {seconds = "0"+seconds;}
+      if (seconds < 10) {seconds = "0"+seconds*1;}
       return minutes+':'+seconds;
     }
 
