224d76f1005905cf4104b709722ea6427849f11b
liebeskind <daniel.liebeskind@gmail.com>
2016-10-04 20:55:21 -0400

    Small fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -315,7 +315,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '**/*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/public/assets/images'
         }]
       }
